124de3996a8b541ae28e98733cb33790dae9a5fa
anup poudel <anup.poudel@tai.com.np>
2023-09-19 21:22:19 +0545

    removed app from docker-compose.yml

--- utils/nginxUtils.js
+++ utils/nginxUtils.js
@@ -2,6 +2,7 @@
 const fs = require('fs');
 const path = require('path');
 const { spawn } = require('child_process');
+const { stdout } = require('process');
 
 function createNginxConfig(hostname) {
     const nginxTemplate = fs.readFileSync(path.join(__dirname, 'docker-data/nginx/conf.d/template'), 'utf8');
@@ -9,10 +10,11 @@ function createNginxConfig(hostname) {
         .replace(/HOSTNAME/g, hostname);
     fs.writeFileSync(path.join(__dirname,`docker-data/nginx/conf.d/${hostname}.conf`), nginxConfig);
 
-    // Reload Nginx to apply the new configuration (requires elevated permissions)
-    const nginxReloadProcess = spawn('docker', ['restart', 'nginx']);
-
+    const nginxReloadProcess = spawn('docker', ['exec', 'wp1c_nginx', 'nginx', '-s', 'reload']);
+    
+    
     nginxReloadProcess.on('close', (code) => {
+        console.log(code)
         if (code === 0) {
             console.log('Nginx configuration reloaded successfully.');
         } else {
